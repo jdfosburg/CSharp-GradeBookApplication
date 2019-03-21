0139e317a13d6f741ae75dac91287edd68eee840
Jeremy Fosburg <jdfosburg2@gmail.com>
2019-03-21 09:17:56 -0500

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -47,6 +47,7 @@ namespace GradeBook.GradeBooks
             if (Students.Count < 5)
             {
                 Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
             }
 
             base.CalculateStudentStatistics(name);
